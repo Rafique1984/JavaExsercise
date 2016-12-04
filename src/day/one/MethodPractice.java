3d7f866688bbffffb455e3497344f6bd59cd4116
Mohammad Islam <rafi420du@gmail.com>
2016-12-03 22:01:35 -0500

    method

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -1,5 +1,19 @@
 package day.one;
 
 public class MethodPractice {
+	public static void main(String []args){
+		System.out.println(getNewName("name"));
+		
+	}
+		// Write a method that take your name input and return your name
+		public static String getNewName(String name){
+			String myName = null;
+			myName = name;
+			
+			
+			return myName;
+		}
+		
+	
 
 }
