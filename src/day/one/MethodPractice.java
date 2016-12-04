e114d4bcbf7bf10b744f00f518b48b6bc5e0075d
Mohammad Islam <rafi420du@gmail.com>
2016-12-03 22:40:48 -0500

    method three

--- src/day/one/MethodPractice.java
+++ src/day/one/MethodPractice.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 public class MethodPractice {
 	public static void main(String []args){
 		System.out.println(getNewName("name"));
-		System.out.println("ans# 2 = "+ getEvenNumberList(20));
+		System.out.println( getEvenNumberList(20));
+		System.out.println("ans# 3 = " + getOddNumberList(30));
+
 	}
 		// Write a method that take your name input and return your name
 		public static String getNewName(String name){
@@ -30,7 +32,18 @@ public class MethodPractice {
 			
 			return evenNumber;
 		}
-		
+		//Write a method that that take a number input and return the list of all odd number from 0 to that given number
+		public static ArrayList<Integer> getOddNumberList(int number) {
+			ArrayList<Integer> oddList = new ArrayList<Integer>();
+			for (int i = 0; i <= number; i++) {
+				if (i % 2 == 1) {
+					oddList.add(i);
+				}
+
+			}
+
+			return oddList;
+		}		
 	
 
 }
